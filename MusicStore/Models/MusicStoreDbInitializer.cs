8465c2ecdf2a3b3d5a631674370e3191488a3008
Kris Hahn <kris.hahn@javs.com>
2017-09-20 20:40:40 -0400

    Add Project to Repo

---
+++ MusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreDbInitializer: DropCreateDatabaseAlways<MusicStoreDB>
+    //public class MusicStoreDbInitializer: CreateDatabaseIfNotExists<MusicStoreDB> // DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+            
+
+            context.Artists.Add(new Artist { Name = "Bon Jovi" });
+            context.Genres.Add(new Genre { Name = "80s Hair Band" });
+            // NOTE: This was causing issues because it didn't have enough information when creating the record in the database.
+            //       And was causing the error in the StoreManagerController.Index method.
+            //context.Albums.Add(new Album  {Title = "New Jersey"});
+
+            context.Albums.Add(new Album()
+            {
+                Artist = new Artist { Name = "Celine Dion" },
+                Genre = new Genre { Name = "Soft Rock" },
+                Price = 9.99m,
+                Title = "The Colour of My Love"
+            });
+
+            // Kris Added
+            context.Albums.Add(new Album()
+            {
+                Artist = new Artist { Name = "Petra" },
+                Genre = new Genre { Name = "Christian Rock" },
+                Price = 9.99m,
+                Title = "Petra World Tour"
+            });
+
+            base.Seed(context);
+        }
+    }
+}
